fff165d414970f1ae9c055a60107a12b9d653a1a
Michael Weigle <michael.weigle@gmail.com>
2017-03-27 23:28:17 -0400

    welp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     $('#markov-well').html(data.markov);
     render_word_cloud(data.wordcloud);
   }).on('ajax:error', function(event, xhr, status, error) {
-    $('#rss_form').after("<br/><div class='alert alert-danger alert-dismissible fade in' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4>Oh no!</h4><p>We couldn't process that feed. Try another one.</p></div>"");
+    $('#rss_form').after("<br/><div class='alert alert-danger alert-dismissible fade in' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4>Oh no!</h4><p>We couldn't process that feed. Try another one.</p></div>");
   });
 });
 
