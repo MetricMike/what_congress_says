a4e68ef4dbc6a77a9cba3000fdc10d47982e835b
Michael Weigle <michael.weigle@gmail.com>
2017-03-31 17:29:55 -0400

    move wordcloud back out to webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,4 +6,21 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-require('bootstrap-loader')
+require('bootstrap-loader');
+const WordCloud = require('wordcloud');
+
+$(document).ready(function() {
+  $('#rss_form').on('ajax:success', function(event, data, status, xhr) {
+    $('#markov-well').html(data.markov);
+    render_word_cloud(data.wordcloud);
+  }).on('ajax:error', function(event, xhr, status, error) {
+    $('#rss_form').after("<br/><div class='alert alert-danger alert-dismissible fade in' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><h4>Oh no!</h4><p>We couldn't process that feed. Try another one.</p></div>");
+  });
+});
+
+function render_word_cloud(token_freqs) {
+  WordCloud($('#wordcloud-well')[0], {
+    shape: 'square',
+    list: token_freqs
+  });
+}
